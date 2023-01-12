a9a43f655fe27cbf8ce67061d172d68454340835
Kamilla Youver / Archisha <95626487+archisha69@users.noreply.github.com>
2023-01-12 16:10:50 +0800

    Add files via upload

---
+++ physics solver/constants.py
@@ -0,0 +1,9 @@
+#-----constants-----#
+l_f = 334000 #J kg-1 (specific latent heat of fusion)
+l_v = 2260000 #J kg-1 (specific latent heat of vaporization)
+c_w = 4200 #J kg-1 degC-1 (specific heat capacity of water)
+abszero = 0 #K (absolute zero)
+N_A = 6.02 * (10 ** 23) #(avogadro's const)
+R = 8.31 #J mol-1 K-1 (universal gas const)
+g = 9.80665 #m s-2 (earth's gravital acceleration)
+#-----end of constants-----#
