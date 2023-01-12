2d4d232f0bdf2cc96a504d9f35018ce8c54229d5
Kamilla Youver / Archisha <95626487+archisha69@users.noreply.github.com>
2023-01-12 19:06:32 +0800

    Add files via upload

--- physics solver.py
+++ physics solver.py
@@ -240,7 +240,7 @@ def slh(l : str = None, Q : str = None, m : str = None):
         #base unit: kg
         return print(f"{'%.5g' % (m)} kg or {'%.5g' % (m / 1000)} g")
 
-#total energy
+#total energy (E = ml + mcΔT)
 def te(E : str = None, m : str = None, l : str = None, c : str = None, dT : str = None):
     if E == None: pass
     elif E.endswith("MJ") or E.endswith("mj") or E.endswith("MW h") or E.endswith("mw h"):
@@ -308,6 +308,77 @@ def te(E : str = None, m : str = None, l : str = None, c : str = None, dT : str 
     
     elif E != None and m != None and l != None and c != None and dT == None:
         dT = ((E / m) - l) / c
+        #base unit: degree celsius
+        return print(f"{'%.5g' % (dT)} °C")
+
+#total energy2 (E = ml + CΔT)
+def te2(E : str = None, m : str = None, l : str = None, C : str = None, dT : str = None):
+    if E == None: pass
+    elif E.endswith("MJ") or E.endswith("mj") or E.endswith("MW h") or E.endswith("mw h"):
+        E = float(E.split()[0]) * 1000000
+    elif E.endswith("kJ") or E.endswith("kj") or E.endswith("kW h") or E.endswith("kw h"):
+        E = float(E.split()[0]) * 1000
+    elif E.endswith("J") or E.endswith("j") or E.endswith("W h") or E.endswith("w h"):
+        E = float(E.split()[0])
+    else:
+        E = float(E)
+
+    if m == None: pass
+    elif m.endswith("kg"):
+        m = float(m.split()[0])
+    elif m.endswith("g"):
+        m = float(m.split()[0]) / 1000
+    else:
+        m = float(m)
+
+    if l == None: pass
+    elif l.endswith("MJ kg-1") or l.endswith("mj kg-1"):
+        l = float(l.split()[0]) * 1000000
+    elif l.endswith("kJ kg-1") or l.endswith("kj kg-1"):
+        l = float(l.split()[0]) * 1000
+    elif l.endswith("J kg-1") or l.endswith("j kg-1"):
+        l = float(l.split()[0])
+    else:
+        l = float(l)
+
+    if C == None: pass
+    elif C.endswith("MJ degC-1") or C.endswith("mj degc-1"):
+        C = float(C.split()[0]) * 1000000
+    elif C.endswith("kJ degC-1") or C.endswith("kj degc-1"):
+        C = float(C.split()[0]) * 1000
+    elif C.endswith("J degC-1") or C.endswith("j degc-1"):
+        C = float(C.split()[0])
+    else:
+        C = float(C)
+
+    if dT == None: pass
+    elif dT.endswith("degC") or dT.endswith("degc"):
+        dT = float(dT.split()[0])
+    else:
+        dT = float(dT)
+
+    if E == None and m != None and l != None and C != None and dT != None:
+        E = (m * l) + (C * dT)
+        #base unit: joule
+        return print(f"{'%.5g' % (E / 1000000)} MJ or {'%.5g' % (E / 1000)} kJ or {'%.5g' % (E)} J")
+
+    elif E != None and m == None and l != None and C != None and dT != None:
+        m = (E - (C * dT)) / l
+        #base unit: kg
+        return print(f"{'%.5g' % (m)} kg or {'%.5g' % (m / 1000)} g")
+    
+    elif E != None and m != None and l == None and C != None and dT != None:
+        l = (E - (C * dT)) / m
+        #base unit: joule kg-1
+        return print(f"{'%.5g' % (l / 1000000)} MJ kg-1 or {'%.5g' % (l /1000)} kJ kg-1 or {'%.5g' % (l)} J kg-1")
+    
+    elif E != None and m != None and l != None and C == None and dT != None:
+        C = (E - (m * l)) / dT
+        #base unit: joule kg^-1 degree celsius^-1
+        return print(f"{'%.5g' % (C / 1000000)} MJ kg-1 °C-1 or {'%.5g' % (C /1000)} kJ kg-1 °C-1 or {'%.5g' % (C)} J kg-1 °C-1")
+    
+    elif E != None and m != None and l != None and C != None and dT == None:
+        dT = (E - (m * l)) / C
         #base unit: degree celsius
         return print(f"{'%.5g' % (dT)} °C")
 
